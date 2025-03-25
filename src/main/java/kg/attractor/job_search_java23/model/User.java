cfc4cb5bc1675f33e4032608fdc71767c32b26ac
nargiza-jd <supabaevanargiza@gmail.com>
2025-03-25 14:40:05 +0600

    Created User

---
+++ src/main/java/kg/attractor/job_search_java23/model/User.java
@@ -0,0 +1,24 @@
+package kg.attractor.job_search_java23.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private int id;
+    private String username;
+    private String email;
+    private String phone;
+    private String profileImageUrl;
+    private String role;
+
+    @Override
+    public String toString() {
+        return String.format("Пользователь: %s (%s)", username, role);
+    }
+}
