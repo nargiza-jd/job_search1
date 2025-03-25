f0ed6e8502e23e18cc0904fe4a3136ee58874618
nargiza-jd <supabaevanargiza@gmail.com>
2025-03-25 14:18:12 +0600

    Created SwaggerConfig class and modified VacancyController

---
+++ src/main/java/kg/attractor/job_search_java23/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package kg.attractor.job_search_java23.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Job Search API")
+                                .version("1.2.3")
+                );
+    }
+}
