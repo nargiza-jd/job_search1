4625ed53472c5b50add1b47d4a93a6d935c567f9
nargiza-jd <supabaevanargiza@gmail.com>
2025-03-25 15:06:49 +0600

    Created ResumeService

---
+++ src/main/java/kg/attractor/job_search_java23/service/ResumeService.java
@@ -0,0 +1,13 @@
+package kg.attractor.job_search_java23.service;
+
+import kg.attractor.job_search_java23.dto.ResumeDto;
+
+import java.util.List;
+
+public interface ResumeService {
+    List<ResumeDto> getAllResumes();
+
+    ResumeDto getResumeById(String resumeId);
+
+    void createResume(ResumeDto resumeDto);
+}
