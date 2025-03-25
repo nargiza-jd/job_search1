3503f8fab094a76e4b0a3af2fbb9255009f19838
nargiza-jd <supabaevanargiza@gmail.com>
2025-03-25 14:47:04 +0600

    Created Responce

---
+++ src/main/java/kg/attractor/job_search_java23/model/Response.java
@@ -0,0 +1,23 @@
+package kg.attractor.job_search_java23.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response {
+    private int id;
+    private int resumeId;
+    private int vacancyId;
+    private int applicantId;
+    private int employerId;
+
+    @Override
+    public String toString() {
+        return String.format("Отклик на вакансию #%d от соискателя #%d", vacancyId, applicantId);
+    }
+}
