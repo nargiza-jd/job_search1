6b9a53cd4b3c5215ad62f47ad0e2f2b0fd85f220
nargiza-jd <supabaevanargiza@gmail.com>
2025-03-25 13:12:10 +0600

    create project

---
+++ src/main/java/kg/attractor/job_search_java23/JobSearchJava23Application.java
@@ -0,0 +1,13 @@
+package kg.attractor.job_search_java23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearchJava23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobSearchJava23Application.class, args);
+    }
+
+}
---
+++ src/test/java/kg/attractor/job_search_java23/JobSearchJava23ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.job_search_java23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchJava23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
