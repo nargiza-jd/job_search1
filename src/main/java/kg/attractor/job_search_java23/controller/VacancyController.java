87d41752cabb923262e793d0216b788701c936a4
nargiza-jd <supabaevanargiza@gmail.com>
2025-03-25 14:32:03 +0600

    Modified VacancyController

--- src/main/java/kg/attractor/job_search_java23/controller/VacancyController.java
+++ src/main/java/kg/attractor/job_search_java23/controller/VacancyController.java
@@ -1,5 +1,6 @@
 package kg.attractor.job_search_java23.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -8,13 +9,17 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("vacancies")
 @RequiredArgsConstructor
 public class VacancyController {
     @GetMapping
-    public HttpStatus index() {
-        return HttpStatus.OK;
+    public ResponseEntity<?> getVacancies() {
+        return ResponseEntity.ok("Vacancies");
     }
 
+    @GetMapping("{vacancyId}")
+    public ResponseEntity<String> getVacancy(@PathVariable("vacancyId") String vacancyId) {
+        return ResponseEntity.ok(vacancyId);
+    }
 
 }
