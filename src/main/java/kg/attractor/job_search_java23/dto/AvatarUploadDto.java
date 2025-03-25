49c6a9a9f7273aee337e22ba825702fdebf74f4a
nargiza-jd <supabaevanargiza@gmail.com>
2025-03-25 18:12:25 +0600

    Create AvatarUploadDto

---
+++ src/main/java/kg/attractor/job_search_java23/dto/AvatarUploadDto.java
@@ -0,0 +1,15 @@
+package kg.attractor.job_search_java23.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AvatarUploadDto {
+    private MultipartFile file;
+}
