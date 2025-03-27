4f778237da3ec87b86755314ba1703c79458b4fb
nargiza-jd <supabaevanargiza@gmail.com>
2025-03-27 07:54:13 +0600

    Created GlobalControllerAdvice

---
+++ src/main/java/kg/attractor/job_search_java23/exceptions/advice/GlobalControllerAdvice.java
@@ -0,0 +1,9 @@
+package kg.attractor.job_search_java23.exceptions.advice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class GlobalControllerAdvice {
+}
