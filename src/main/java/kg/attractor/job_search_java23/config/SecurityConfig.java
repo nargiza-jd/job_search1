b062316dd10c44fbdee71505ea09b9b347584d4e
nargiza-jd <supabaevanargiza@gmail.com>
2025-03-29 13:50:45 +0600

    Added 004_authorities_yaml

--- src/main/java/kg/attractor/job_search_java23/config/SecurityConfig.java
+++ src/main/java/kg/attractor/job_search_java23/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package kg.attractor.job_search_java23.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,31 +12,42 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    private final DataSource dataSource;
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public InMemoryUserDetailsManager inMemoryUserDetails() {
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("ADMIN")
+//                .authorities("FULL")
+//                .build();
+//
+//        UserDetails user = User.builder()
+//                .username("user")
+//                .password(passwordEncoder().encode("qwe"))
+//                .roles("GUEST")
+//                .authorities("READ_ONLY")
+//                .build();
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource);
     }
 
-    @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetails() {
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .authorities("FULL")
-                .build();
 
-        UserDetails user = User.builder()
-                .username("user")
-                .password(passwordEncoder().encode("qwe"))
-                .roles("GUEST")
-                .authorities("READ_ONLY")
-                .build();
-        return new InMemoryUserDetailsManager(admin, user);
-    }
 }
