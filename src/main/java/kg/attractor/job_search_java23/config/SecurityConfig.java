f95e763915a9701b1000e0221c3c8ce2d66a1208
nargiza-jd <supabaevanargiza@gmail.com>
2025-03-29 13:21:56 +0600

    Added SecurityConfig

--- src/main/java/kg/attractor/job_search_java23/config/SecurityConfig.java
+++ src/main/java/kg/attractor/job_search_java23/config/SecurityConfig.java
@@ -1,4 +1,40 @@
 package kg.attractor.job_search_java23.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetails() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .authorities("FULL")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("qwe"))
+                .roles("GUEST")
+                .authorities("READ_ONLY")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
 }
